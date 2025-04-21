9930a80f49d732658d1ef9ba4125fab2d3948381
Deven Gaikwad <gdeven13@gmail.com>
2025-04-21 11:11:38 +0530

    Add files via upload

---
+++ Grade Calculator.cpp
@@ -0,0 +1,54 @@
+
+#include<iostream> 
+using namespace std; 
+ 
+int main(){ 
+
+cout<< " ****** Examination: Grade Card (2024 -2025) ****** "<<endl;
+cout<<endl;
+cout<< "University   : DES Pune, University"<<endl;
+cout<< "College      : School of Engineering and Technology"<<endl;
+cout<<endl;
+cout<< "Student Name : Deven Sachin Gaikwad"<<endl;
+cout<< "PRN NO       : 1022411052"<<endl;
+cout<< "Course       : BTech Electronics and Communication Engineering (AIML)"<<endl;
+cout<< "Semester     : 1 (FY)"<<endl;
+cout<<endl;
+cout<< "Subject         : Design Thinking"<<endl;
+cout<< "Theory Marks    : 22 / 25"<<endl;
+cout<< "Practical Marks : 18 / 25"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Subject         : Probability and Statistics"<<endl;
+cout<< "Theory Marks    : 37 / 50"<<endl;
+cout<< "Practical Marks : 49 / 50"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Subject         : Fundamentals of Computer Programming"<<endl;
+cout<< "Theory Marks    : 42 / 50"<<endl;
+cout<< "Practical Marks : 45 / 50"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Subject         : Indian Knowledge Sysytem - I"<<endl;
+cout<< "Practical Marks : 47 / 50"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Subject         : Yoga and Meditation - I"<<endl;
+cout<< "Practical Marks : 47 / 50"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Subject         : Basics of Electrical Engineering"<<endl;
+cout<< "Theory Marks    : 45 / 50"<<endl;
+cout<< "Practical Marks : 40 / 50"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Subject         : Manufacturing Practices"<<endl;
+cout<< "Practical Marks : 44 / 50"<<endl;
+cout<< "Result Status   : PASS"<<endl;
+cout<<endl;
+cout<< "Percentage      : 84.00 %"<<endl;
+cout<< "SGPA            : 9.2"<<endl;
+cout<< "Overall Result Status   : PASS :)"<<endl;
+
+return 0; 
+}
